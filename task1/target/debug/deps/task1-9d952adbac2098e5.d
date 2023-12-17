4456ad2fa3a4aef543f5a89c088957331e158e32
Nesim Özgün <78825897+nesimozgun@users.noreply.github.com>
2023-12-17 19:39:02 +0300

    concatenate strings

--- task1/target/debug/deps/task1-9d952adbac2098e5.d
+++
@@ -1,5 +0,0 @@
-/Users/nesimozgun/workspace/RustBootcamp/task1/target/debug/deps/task1-9d952adbac2098e5: src/main.rs
-
-/Users/nesimozgun/workspace/RustBootcamp/task1/target/debug/deps/task1-9d952adbac2098e5.d: src/main.rs
-
-src/main.rs:
