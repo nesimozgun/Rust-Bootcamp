ecfabd773730718235725a05af449354757a2889
nesimozgun <ozgunnesim@gmail.com>
2023-12-17 19:25:48 +0300

    concatenate strings

--- task1/src/main.rs
+++ task1/src/main.rs
@@ -1,13 +1,15 @@
+// create a function which takes two string as arguments and returns a string
 fn concatenate_strings(a: &str, b: &str) -> String {
-    let mut result = String::new();
-    result.push_str(a);
-    result.push_str(b);
-    result
+    let mut result = String::new(); // mutable result variable
+    result.push_str(a); // pushing a to result
+    result.push_str(b); // pushing b to result
+    result // return result
 }
 
+// implement the main function
 fn main() {
-    let string1 = String::from("string one ");
-    let string2 = String::from("string two ");
-    let concatenated_string = concatenate_strings(&string1[..], &string2[..]);
-    println!("Concatenated string: {}", concatenated_string);
+    let string1 = String::from("string one "); // create string1 variable
+    let string2 = String::from("string two "); // create string2 variable
+    let concatenated_string = concatenate_strings(&string1[..], &string2[..]); // create a concatenated string variable and assign concatenate_strings function to it
+    println!("Concatenated string: {}", concatenated_string); // print the concatenated string
 }
